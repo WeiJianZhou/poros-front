608b61d7c37515042a1b1b3522e9edcbd5d1aaf2
weijian.zhou <weijian.zhou@getech.cn>
2020-08-12 12:04:03 +0800

    update:dll

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin')
 const system = require('./src/config/system')
 const modifyVars = require('./src/config/theme')
 
@@ -9,6 +11,14 @@ const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
   publicPath: '/' + (isDev ? '' : system.code),
+  configureWebpack: {
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: process.cwd(),
+        manifest: require('./public/vendor/vendor-manifest.json')
+      })
+    ]
+  },
   chainWebpack(config) {
     config.plugin('html').tap(args => {
       args[0].title = system.name
@@ -16,6 +26,10 @@ module.exports = {
       args[0].isDev = isDev
       return args
     })
+    config.plugins.delete('prefetch')
+    // ie兼容
+    config.entry('main').add('babel-polyfill')
+    // 处理svg图片
     config.module
       .rule('svg')
       .exclude.add(resolve('src/assets/svg'))
@@ -31,6 +45,17 @@ module.exports = {
         symbolId: 'icon-[name]'
       })
       .end()
+
+    if (!isDev) {
+      // 开启gzip
+      config.plugin('compressionPlugin')
+        .use(new CompressionPlugin({
+          test:/\.js$|\.html$|.\css/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false // 不删除源文件
+        }))
+        .end()
+    }
   },
   css: {
     loaderOptions: {
