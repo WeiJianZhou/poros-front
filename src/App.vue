105abbd03b9caef0a5f9af620a8c03a0661d614d
weijian.zhou <weijian.zhou@getech.cn>
2020-08-12 13:28:28 +0800

    update:app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <p-config-provider>
-    <div id="app">
-      <router-view />
-    </div>
-  </p-config-provider>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <style lang="less">
