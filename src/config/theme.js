53e9fed1b78e6e6493d9a12c783a024c378d7059
weijian.zhou <weijian.zhou@getech.cn>
2020-08-13 18:05:40 +0800

    update:theme.js

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,3 +1,10 @@
 module.exports = {
-  'primary-color': '#4064DF' // 主题色
+  'primary-color': '#4064DF',                             // 主题色
+  'success-color': '#52c41a',                             // success
+  'warning-color': '#faad14',                             // warning
+  'error-color': '#f5222d',                               // error
+  'sider-menu-bg': '@primary-color',                      // 左侧菜单背景
+  'sider-menu-logo-bg': 'rgba(256,256,256, 0.072)',       // 左侧菜单顶部logo背景
+  'sider-menu-font-color': '#FFFFFF',                     // 左侧菜单字体颜色
+  'sider-menu-selectd-bg': 'rgba(0, 0, 0, 0.2)'           // 左侧菜单选中背景
 }
